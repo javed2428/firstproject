53f8d1bfcf439c2ded2972ba4c2c0e2f81d2751f
Javed <javed@91mobiles.com>
2015-06-27 11:08:45 +0530

    formatted o/p file to be exported to elastic search created

--- script_for_formatted_json_file.php
+++ script_for_formatted_json_file.php
@@ -1,10 +1,6 @@
 <?php
 $jsonString = file_get_contents('./amazon-crawler/output.json');
-$data_arr = json_decode($jsonString);
-#echo "<pre>";
-#print_r($data_arr);
-#echo "</pre>";
-$count = 1;
+$data_arr = json_decode($jsonString,true);
 
 //$index = array()
 //{"index":{"_id":"1"}}
@@ -18,6 +14,28 @@ $count = 1;
 //json_encode($index);
 // write_to_file()
 
+//{"index":{"_id":"1"}}
 
+$count = 1;
+
+//echo $doc1;
+$new_arr=[];
+foreach($data_arr as $document){
+
+    $arr = ['index'=>['_id'=>"{$count}"]];
+//    $doc1 = json_encode($arr);
+//    $value = json_decode($doc1);
+    $new_arr[] = $arr;
+    $new_arr[] = $document;
+    $count++;
+}
+
+$newJsonString = json_encode($new_arr);
+$newJsonString = str_replace('},{"price"', '}'."\n".'{"price"', $newJsonString);
+$newJsonString = str_replace('},{"index"', '}'."\n".'{"index"', $newJsonString);
+
+
+//echo $newstring;
+file_put_contents('formatted_output.json',$newJsonString);
 
 ?>

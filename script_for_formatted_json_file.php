f4839fbded7df4fb19ee27d3261a8bc1b647e93f
Javed <javed@91mobiles.com>
2015-06-27 09:30:12 +0530

    from now onwards i'll try to use json encode

--- script_for_formatted_json_file.php
+++ script_for_formatted_json_file.php
@@ -1,4 +1,23 @@
 <?php
+$jsonString = file_get_contents('./amazon-crawler/output.json');
+$data_arr = json_decode($jsonString);
+#echo "<pre>";
+#print_r($data_arr);
+#echo "</pre>";
+$count = 1;
+
+//$index = array()
+//{"index":{"_id":"1"}}
+//arr = array()
+//$arr['id'] = "1"
+//
+//
+//{'id':'1'}
+//
+//$index['index'] = $arr;
+//json_encode($index);
+// write_to_file()
+
 
 
 ?>
